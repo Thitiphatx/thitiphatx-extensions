edcdd3553d554cfc9a23c669a0bedd07b6a4c6f6
Thitiphat <thitiphat6391@gmail.com>
2023-04-08 14:58:03 +0700

    versioning

--- src/Niceoppai/Niceoppai.ts
+++ src/Niceoppai/Niceoppai.ts
@@ -29,7 +29,7 @@ import {
 const NO_DOMAIN = 'https://www.niceoppai.net'
 
 export const NiceoppaiInfo: SourceInfo = {
-    version: '1.0.4',
+    version: '1.0.5',
     name: 'Niceoppai',
     icon: 'icon.png',
     author: 'Thitiphatx',
