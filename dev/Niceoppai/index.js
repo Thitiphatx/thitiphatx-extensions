876f6cbc7eb4eea30c8440d003b4d3434c0af32c
Thitiphatx <37146306+Thitiphatx@users.noreply.github.com>
2023-05-10 13:05:59 +0000

    Deploying to gh-pages from @ Thitiphatx/thitiphatx-extensions@f6c180d137c52e04bd0aa4a84bddb85a800d0e86 🚀

--- dev/Niceoppai/index.js
+++ dev/Niceoppai/index.js
@@ -1216,24 +1216,6 @@ const parseUpdatedManga = ($, time, ids) => {
 exports.parseUpdatedManga = parseUpdatedManga;
 const parseHomeSections = ($, sectionCallback) => {
     const latestSection = createHomeSection({ id: 'latest_comic', title: 'Latest Manga', view_more: true });
-    const popularSection = createHomeSection({ id: 'popular_comic', title: 'Popular Manga', view_more: false });
-    const popularSection_Array = [];
-    for (const comic of $('div.nde', 'li.wid.widget_text div.con div.textwidget div.wpm_pag.mng_lts_chp.tbn').toArray()) {
-        let image = encodeURI($('div.cvr > div > a > img', comic).attr('src').replace("62x88", "350x0")) ?? '';
-        const title = $('div.det div.ifo a.ttl', comic).first().text().trim() ?? '';
-        const id = $('div.det div.ifo a.ttl', comic).attr('href').split('/')[3] ?? '';
-        const subtitle = $('div.det div.ifo span.chp_ifo span.lng_', comic).first().text().trim() ?? '';
-        if (!id || !title)
-            continue;
-        popularSection_Array.push(createMangaTile({
-            id: id,
-            image: image,
-            title: createIconText({ text: decodeHTMLEntity(title) }),
-            subtitleText: createIconText({ text: subtitle }),
-        }));
-    }
-    popularSection.items = popularSection_Array;
-    sectionCallback(popularSection);
     const latestSection_Array = [];
     for (const comic of $('div.row', 'div.wpm_pag.mng_lts_chp.grp').toArray()) {
         let image = encodeURI($('div.cvr > div > a > img', comic).first().attr('src').replace("36x0", "350x0")) ?? '';
